e94c1803e07e4f47da97c3f0ab6aefa462fc7eab
unknown <dvlasic000@.gmail.com>
2023-03-10 12:35:39 +0000

    lab6

---
+++ src/app/student.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import{Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentService {
+
+  constructor(private http:HttpClient) { }
+
+  getStudentData():Observable<any>{
+    return this.http.get('https://www.jsonblob.com/api/jsonblob/1083709262370455552');
+  }
+
+  getWeatherData():Observable<any>{
+    return this.http.get('http://api.openweathermap.org/data/2.5/weather?q=Galway&appid=6a66416403ed8e5e6e762cb8c261f303');
+  }
+}
